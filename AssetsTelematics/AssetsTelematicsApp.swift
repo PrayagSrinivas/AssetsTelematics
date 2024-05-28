847f762714990a2fda80646743555a3ba66c6125
Srinivas prayag sahu <78862797+PrayagSrinivas@users.noreply.github.com>
2024-05-28 23:00:07 +0530

    Initial Commit

---
+++ AssetsTelematics/AssetsTelematicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsTelematicsApp.swift
+//  AssetsTelematics
+//
+//  Created by Srinivas Prayag Sahu on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AssetsTelematicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
